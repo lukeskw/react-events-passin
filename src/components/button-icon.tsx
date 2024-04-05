d9557306e2119c344cbb50c0c5035d3caf2e5f5e
lukeskw <lucasporfirioa@gmail.com>
2024-04-05 15:54:02 -0300

    refactor: refactoring button-icon to use twMerge

--- src/components/button-icon.tsx
+++ src/components/button-icon.tsx
@@ -1,4 +1,5 @@
 import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 interface ButtonIconProps extends ComponentProps<'button'> {
   variant?: 'default' | 'transparent'
@@ -8,11 +9,16 @@ export function ButtonIcon({ variant = 'default', ...props }: ButtonIconProps) {
   return (
     <button
       {...props}
-      className={
-        variant === 'transparent'
-          ? 'rounded-md border border-white/10 bg-black/20 p-1.5'
-          : 'rounded-md border border-white/10 bg-white/10 p-1.5'
-      }
+      // className={
+      //   variant === 'transparent'
+      //     ? 'rounded-md border border-white/10 bg-black/20 p-1.5'
+      //     : 'rounded-md border border-white/10 bg-white/10 p-1.5'
+      // }
+      className={twMerge(
+        'rounded-md border border-white/10 p-1.5',
+        variant === 'transparent' ? 'bg-black/20' : 'bg-white/10',
+        props.disabled ? 'opacity-50' : null,
+      )}
     />
   )
 }
